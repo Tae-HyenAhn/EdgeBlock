609cf4fd34a3ba859c4942ec3f48de8a992c9b6c
DESKTOP-UDTPRLP\Taehyen <thahn88@naver.com>
2017-06-07 13:18:28 +0900

    Butterknife 적용, SMALL 아이콘 변경

--- app/src/main/java/com/edgeblock/edgeblock/BlockingService.java
+++ app/src/main/java/com/edgeblock/edgeblock/BlockingService.java
@@ -1,18 +1,14 @@
 package com.edgeblock.edgeblock;
 
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
-import android.os.Handler;
 import android.os.IBinder;
-import android.support.annotation.IntDef;
 import android.support.v4.app.NotificationCompat;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -21,17 +17,28 @@ import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+/**
+ * BlockingService.java
+ * @author Taehyen
+ *
+ */
 public class BlockingService extends Service {
+    /**
+     * Notification 코드
+     */
     private static final int NOTI_CODE = 39;
 
-    private NotificationManager nm;
     private NotificationCompat.Builder mBuilder;
     private Intent intent;
     private PendingIntent pIntent;
 
     private WindowManager windowManager;
 
+    /**
+     * 터치를 막는 레이아웃
+     */
     private LinearLayout leftLayout, rightLayout;
+
     private Notification noti;
     public BlockingService() {
     }
@@ -41,7 +48,7 @@ public class BlockingService extends Service {
         super.onCreate();
 
         DisplayMetrics dm = getResources().getDisplayMetrics();
-        int areaWidth = (int)(((double)dm.widthPixels)*0.04);
+        int areaWidth = getBlockWidth();
 
         final WindowManager.LayoutParams mParamsLeft = new WindowManager.LayoutParams(
                 areaWidth, WindowManager.LayoutParams.MATCH_PARENT,
@@ -68,7 +75,7 @@ public class BlockingService extends Service {
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         windowManager.addView(leftLayout, mParamsLeft);
         windowManager.addView(rightLayout, mParamsRight);
-        
+
 
         Toast.makeText(getApplicationContext(), "On", Toast.LENGTH_SHORT).show();
     }
@@ -77,6 +84,7 @@ public class BlockingService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         turnOnNotification();
+        //서비스를 죽지 않도록 하기 위해 foreground 실행
         startForeground(NOTI_CODE, noti);
 
         return Service.START_STICKY;
@@ -99,6 +107,18 @@ public class BlockingService extends Service {
         super.onDestroy();
     }
 
+    /**
+     * 화면 너비의 4퍼센트 씩 양쪽 을 막는다
+     * @return 화면 너비의 4퍼센트로 계산되어 나온 값
+     */
+    private int getBlockWidth(){
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        return (int)(((double)dm.widthPixels)*0.04);
+    }
+
+    /**
+     * Notificationn 설정
+     */
     private void turnOnNotification(){
 
         intent = new Intent(getApplicationContext(), MainActivity.class);
@@ -106,10 +126,10 @@ public class BlockingService extends Service {
 
         Bitmap iconBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
         mBuilder = new NotificationCompat.Builder(getApplicationContext())
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.mipmap.ic_small)
                 .setLargeIcon(iconBitmap)
-                .setContentTitle("EdgeBlock")
-                .setContentText("Now Blocking...")
+                .setContentTitle(getResources().getString(R.string.noti_title))
+                .setContentText(getResources().getString(R.string.noti_content))
                 .setContentIntent(pIntent);
         noti = mBuilder.build();
         noti.flags = noti.FLAG_NO_CLEAR;
--- app/src/main/java/com/edgeblock/edgeblock/MainActivity.java
+++ app/src/main/java/com/edgeblock/edgeblock/MainActivity.java
@@ -1,20 +1,18 @@
 package com.edgeblock.edgeblock;
 
 import android.Manifest;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v4.app.NotificationCompat;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
+import android.util.Log;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -23,18 +21,34 @@ import com.gun0912.tedpermission.TedPermission;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+
+/**
+ * MainActivity.java
+ * @author Taehyen
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
 
     private static final String SHARED_PREF_KEY = "EDGEBLOCK";
     private static final String KEY_SWITCH = "key_switch";
 
-
-
-    private Switch notiSwitch;
+    /**
+     * View Init..
+     */
+    @BindView(R.id.main_switch) Switch notiSwitch;
+    @BindView(R.id.main_edge_left) LinearLayout leftEdge;
+    @BindView(R.id.main_edge_right) LinearLayout rightEdge;
 
     private boolean initSwitchValue;
 
+    /**
+     * Permission Listener (using TedPermission)
+     */
     PermissionListener permissionlistener = new PermissionListener() {
         @Override
         public void onPermissionGranted() {
@@ -49,36 +63,56 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
-
-    private CompoundButton.OnCheckedChangeListener checkedChangeListener = new CompoundButton.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            if(isChecked){
-
-                blockingStart();
-            }else{
-
-                blockingStop();
-            }
-            savePref(isChecked);
+    @OnCheckedChanged(R.id.main_switch)
+    void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+        Log.d(TAG, "Check Changed..: "+isChecked);
+        if(isChecked){
+            blockingStart();
+        }else{
+            blockingStop();
         }
-    };
+        updateUIEdgeBox(isChecked);
+        savePref(isChecked);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         getPermission();
         init();
     }
 
+    /**
+     * Init
+     */
     private void init(){
         initSwitchValue = getPref();
-        notiSwitch = (Switch) findViewById(R.id.main_switch);
+
         notiSwitch.setChecked(initSwitchValue);
-        notiSwitch.setOnCheckedChangeListener(checkedChangeListener);
+        updateUIEdgeBox(initSwitchValue);
     }
 
+
+    /**
+     * Activate 됬을 때 양쪽 모서리에 실행중인 느낌을 주기위해 박스 생성
+     * @param isActivated is Switch ON??
+     */
+    private void updateUIEdgeBox(boolean isActivated){
+        if(isActivated){
+            leftEdge.setVisibility(View.VISIBLE);
+            rightEdge.setVisibility(View.VISIBLE);
+        }else{
+            leftEdge.setVisibility(View.GONE);
+            rightEdge.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * Get Permission
+     */
     private void getPermission(){
         new TedPermission(this)
                 .setPermissionListener(permissionlistener)
@@ -87,15 +121,25 @@ public class MainActivity extends AppCompatActivity {
                 .check();
     }
 
+    /**
+     * 서비스 실행
+     */
     private void blockingStart(){
         startService(new Intent(this, BlockingService.class));
     }
 
+    /**
+     * 서비스 종료
+     */
     private void blockingStop(){
         stopService(new Intent(this, BlockingService.class));
 
     }
 
+    /**
+     * 상태 저장 ( 다시 실행했을 때 상태에 맞게 UI 갱신하기 위해 )
+     * @param state
+     */
     private void savePref(boolean state){
         SharedPreferences pref = getSharedPreferences(SHARED_PREF_KEY, MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
@@ -103,6 +147,10 @@ public class MainActivity extends AppCompatActivity {
         editor.commit();
     }
 
+    /**
+     * 상태 가져오기 ( 다시 실행했을 때 상태에 맞게 UI 갱신하기 위해 )
+     * @return 상태 bool
+     */
     private boolean getPref(){
         SharedPreferences pref = getSharedPreferences(SHARED_PREF_KEY, MODE_PRIVATE);
         return pref.getBoolean(KEY_SWITCH, false);
